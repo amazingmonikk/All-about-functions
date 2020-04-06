374b37ce7d160f8d65a070e3c4c87e40f7ad0507
amazingmonikk <MONIKK.TADROSS@GMAIL.COM>
2020-04-06 19:51:51 -0400

    Object constructor part 2

---
+++ object_constructor2.js
@@ -0,0 +1,10 @@
+function Car(model, year) {
+    this.carModel = model;
+    this.year = year;
+}
+
+var bmw = new Car("n247", 2004)
+console.log(bmw.carModel)
+
+mercedes = new Car("z675", 2016)
+console.log(mercedes.carModel)
