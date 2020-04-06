3e6ffd83b5b204f87c574a1c7b8e6a786fc199a9
amazingmonikk <MONIKK.TADROSS@GMAIL.COM>
2020-04-06 18:16:03 -0400

    Objects

---
+++ object_basics.js
@@ -0,0 +1,8 @@
+var process = {
+    fullName: "Monika Rossiii",
+    displayName: function () {
+        console.log(this.fullName)
+    }
+}
+// process.displayName()
+console.log(process.fullName)
