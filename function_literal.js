884bffb502b4a30188411637520271cbd8f3addf
amazingmonikk <MONIKK.TADROSS@GMAIL.COM>
2020-04-06 19:11:56 -0400

    Object constructor

--- function_literal.js
+++ function_literal.js
@@ -1,5 +1,11 @@
+// var add = function (nr1, nr2) {
+//     var sum = nr1 + nr2;
+//     console.log(sum)
+// }
+//     (10, 15)
+
 var add = function (nr1, nr2) {
     var sum = nr1 + nr2;
-    console.log(sum)
+    console.log(this)
 }
-    (10, 15)
+    ()
---
+++ object_constructor.js
@@ -0,0 +1,20 @@
+var Car = function () {
+    var model, year;
+    return console.log(this)
+}
+
+bmw = new Object
+bmw.model = '890809'
+bmw.year = '2006'
+
+console.log(bmw.model)
+
+
+
+mercedes = new Object
+mercedes.model = 'c258'
+mercedes.year = '2006'
+
+console.log(mercedes.model)
+
+
