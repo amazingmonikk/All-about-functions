be286b6e634383166d9b56c423a897826ef5cd1a
amazingmonikk <MONIKK.TADROSS@GMAIL.COM>
2020-04-11 18:55:56 -0400

    Return & local and global scope

---
+++ return_statement.js
@@ -0,0 +1,6 @@
+function run(number1, number2) {
+    console.log("Hello beautiful")
+    return number1 + number2
+}
+
+console.log(run(2, 2))
---
+++ scope.js
@@ -0,0 +1,6 @@
+function run() {
+    speed = "100 miles per hour"
+    return speed
+}
+run()
+console.log(speed)
