9b90a9643a77efea5eb62be934ae504bb7493baa
amazingmonikk <MONIKK.TADROSS@GMAIL.COM>
2020-04-12 16:25:36 -0400

    Hoisting

---
+++ hoisting.js
@@ -0,0 +1,13 @@
+function run() {
+
+    var speed;
+    console.log("this car is running " + speed)
+
+    var speed = "100 miles per hour"
+}
+
+run()
+
+
+//when JavaScript compiles all of your code, all variable declarations using var are lifted to the top of their functional/local scope (if declared inside a function) or to the top of their global scope (if declared outside of a function) regardless of where the actual declaration has been made.
+
